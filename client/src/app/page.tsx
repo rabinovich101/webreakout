1fbe9c27816f87ac3e0ea43d34f3008491686824
Oleg Rabinovich <Rabinovich101@gmail.com>
2024-07-30 10:43:44 +0300

    changing things in the page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
     <Container>
       <Hero />
       <SectionTitle
-        preTitle="Nextly Benefits"
-        title=" Why should you use this landing page"
+        preTitle="WeBreakOut Benefits"
+        title=" Why should you use this services"
       >
         Nextly is a free landing page & marketing website template for startups
         and indie projects. Its built with Next.js & TailwindCSS. And its
